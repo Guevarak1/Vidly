6df48b5bcb8129771de10b60db42f07238bdd890
Kevin Guevara <guevarak1@gmail.com>
2017-05-30 16:22:15 -0400

    fusedlocationapi implemented

--- app/src/main/java/com/kevguev/mobile/vidly/ui/MainListFragment.java
+++ app/src/main/java/com/kevguev/mobile/vidly/ui/MainListFragment.java
@@ -6,17 +6,23 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,11 +31,16 @@ import android.widget.Toast;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
 import com.google.api.client.util.ExponentialBackOff;
-import com.google.api.client.util.Strings;
 import com.google.api.services.youtube.YouTubeScopes;
 import com.google.api.services.youtube.model.Video;
 import com.kevguev.mobile.vidly.R;
@@ -46,14 +57,21 @@ import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
 import static android.app.Activity.RESULT_OK;
+import static com.google.android.gms.location.LocationServices.FusedLocationApi;
 
-public class MainListFragment extends Fragment implements EasyPermissions.PermissionCallbacks, SearchAdapter.ItemClickCallback {
+public class MainListFragment extends Fragment
+        implements EasyPermissions.PermissionCallbacks,
+        SearchAdapter.ItemClickCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     FloatingActionButton mFab;
     ProgressDialog mProgress;
     private RecyclerView recView;
     private SearchAdapter adapter;
     private ArrayList listData;
+    private Location lastLocation;
 
     private static final String BUNDLE_EXTRAS = "BUNDLE_EXTRAS";
     private static final String EXTRA_QUOTE = "EXTRA_QUOTE";
@@ -62,10 +80,16 @@ public class MainListFragment extends Fragment implements EasyPermissions.Permis
     static final int REQUEST_AUTHORIZATION = 1001;
     static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
     static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
-    private static final String BUTTON_TEXT = "Call YouTube Data API";
+    static final int REQUEST_PERMISSION_LOCATION = 1004;
     private static final String PREF_ACCOUNT_NAME = "accountName";
 
     GoogleAccountCredential mCredential;
+
+    protected static final String TAG = MainListFragment.class.getSimpleName();
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
+
     private static final String[] SCOPES = {YouTubeScopes.YOUTUBE_READONLY};
 
     public MainListFragment() {
@@ -81,6 +105,27 @@ public class MainListFragment extends Fragment implements EasyPermissions.Permis
                 getActivity().getApplicationContext(), Arrays.asList(SCOPES))
                 .setBackOff(new ExponentialBackOff());
 
+        buildGoogleApiClient();
+        createLocationRequest();
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+
+    private void createLocationRequest() {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setInterval(10 * 1000);
+        mLocationRequest.setFastestInterval(1 * 1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
     }
 
@@ -107,6 +152,36 @@ public class MainListFragment extends Fragment implements EasyPermissions.Permis
         recView.setAdapter(adapter);
 
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+
     }
 
     @Override
@@ -232,7 +307,7 @@ public class MainListFragment extends Fragment implements EasyPermissions.Permis
             //String query = prefs.getString(getString(R.string.pref_search_query), "default query");
 
             String publishedAfter = prefs.getString(getString(R.string.pref_published_after), "day"); // published after 1 day
-            // String location = prefs.getString(getString(R.string.pref_location), "40.7417544,-74.0086348"); //new york
+            // String lastLocation = prefs.getString(getString(R.string.pref_location), "40.7417544,-74.0086348"); //new york
             String radius = prefs.getString(getString(R.string.pref_radius), "1km");
 
             new MakeRequestTask(publishedAfter, locationChosen, radius).execute();
@@ -255,7 +330,6 @@ public class MainListFragment extends Fragment implements EasyPermissions.Permis
 
     private void createDialog() {
 
-
         new MaterialDialog.Builder(getActivity())
                 .title(R.string.dialog_location_title)
                 .items(R.array.currentLocationValues)
@@ -277,7 +351,8 @@ public class MainListFragment extends Fragment implements EasyPermissions.Permis
         String[] locationList = getResources().getStringArray(R.array.locations);
         switch (i) {
             case 0:
-                Toast.makeText(getActivity(), "eventually current location", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "eventually current lastLocation", Toast.LENGTH_SHORT).show();
+                chosen = lastLocation.getLatitude() + ", " + lastLocation.getLongitude();
                 break;
             case 1:
                 Toast.makeText(getActivity(), locationList[0], Toast.LENGTH_SHORT).show();
@@ -393,6 +468,79 @@ public class MainListFragment extends Fragment implements EasyPermissions.Permis
         //do nothing
     }
 
+    @AfterPermissionGranted(REQUEST_PERMISSION_LOCATION)
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (EasyPermissions.hasPermissions(
+                getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+            lastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (lastLocation == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            } else {
+                handleNewLocation(lastLocation);
+            }
+        }
+        else{
+            // Request the GET_ACCOUNTS permission via a user dialog
+            EasyPermissions.requestPermissions(
+                    this,
+                    "This app needs to access your Google account (via Contacts).",
+                    REQUEST_PERMISSION_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(getActivity(), CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(getActivity(), "location :"+location.getLatitude()+" , "+location.getLongitude(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void handleNewLocation(Location location){
+        Log.d(TAG, location.toString());
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+
+        Toast.makeText(getActivity(), String.valueOf(currentLatitude) + " " + String.valueOf(currentLongitude), Toast.LENGTH_SHORT).show();
+        //LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+    }
     public class MakeRequestTask extends AsyncTask<Void, Void, List<Video>> {
 
         /**
